01ca9fe75afa8f6f335246e8fb91bcea823d4a60
Daniel del Río <d.rio@sixtema.es>
2021-02-07 15:48:33 +0100

    Cambios para el despliegue

---
+++ backend/src/main/java/dh/covid/api/controllers/SitemapController.java
@@ -0,0 +1,59 @@
+package dh.covid.api.controllers;
+
+import dh.covid.api.services.CountryService;
+import dh.covid.api.services.DataDumper;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class SitemapController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
+    @ApiOperation("Fetch data from online csvs and updates DB. ONLY FOR DEV USE")
+    private ResponseEntity<?> testVaccines() throws Exception {
+
+        List<String> countryNames = countryService.findAll().stream().map(country -> country.getName()).collect(Collectors.toList());
+        String today = new Date().toString();
+        String xml = "";
+        xml += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        xml += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
+        xml += "<url>\n" +
+                "\n" +
+                "      <loc>https://vaccine-covid.live/</loc>\n" +
+                "\n" +
+                "      <lastmod>"+today+"</lastmod>\n" +
+                "\n" +
+                "      <changefreq>daily</changefreq>\n" +
+                "\n" +
+                "      <priority>0.8</priority>\n" +
+                "\n" +
+                "   </url>";
+
+        for(String country: countryNames){
+            xml += "<url>\n" +
+                    "\n" +
+                    "      <loc>https://vaccine-covid.live/countries/"+country+"</loc>\n" +
+                    "\n" +
+                    "      <lastmod>"+today+"</lastmod>\n" +
+                    "\n" +
+                    "      <changefreq>daily</changefreq>\n" +
+                    "\n" +
+                    "      <priority>0.5</priority>\n" +
+                    "\n" +
+                    "   </url>\n";
+        }
+        return ResponseEntity.ok(xml);
+    }
+
+}
